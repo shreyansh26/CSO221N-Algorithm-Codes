f63d2b69d87bf99b4df02f923030cc8c4dcad9a6
Shreyansh <shreyansh.pettswood@gmail.com>
2018-02-02 13:57:13 +0530

    Bucket Sort

--- Bucket Sort/bucketsort.cpp
+++ Bucket Sort/bucketsort.cpp
@@ -2,41 +2,35 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
- 
-// Function to sort arr[] of size n using bucket sort
-void bucketSort(float arr[], int n)
-{
-    // 1) Create n empty buckets
+
+void bucketSort(float arr[], int n) {
     vector<float> b[n];
-    
-    // 2) Put array elements in different buckets
-    for (int i=0; i<n; i++)
-    {
-       int bi = n*arr[i]; // Index in bucket
-       b[bi].push_back(arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        int bi = n * arr[i]; 
+        b[bi].push_back(arr[i]);
     }
- 
-    // 3) Sort individual buckets
-    for (int i=0; i<n; i++)
-       sort(b[i].begin(), b[i].end());
- 
-    // 4) Concatenate all buckets into arr[]
+
+    for (int i = 0; i < n; i++)
+        sort(b[i].begin(), b[i].end());
+
     int index = 0;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < b[i].size(); j++)
-          arr[index++] = b[i][j];
+            arr[index++] = b[i][j];
 }
- 
-/* Driver program to test above funtion */
-int main()
-{
-    float arr[] = {0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    bucketSort(arr, n);
- 
-    cout << "Sorted array is \n";
-    for (int i=0; i<n; i++)
-       cout << arr[i] << " ";
-     cout<<"\n";
-    return 0;
+
+int main() {
+    float a[1000];
+    int i, n;
+    printf("Number of elements: ");
+    scanf("%d",&n);
+    printf("Elements: ");
+    for(i = 0; i < n; i++) {
+        scanf("%f",&a[i]);
+    }
+    bucketSort(a, n);
+    for(i = 0; i < n; i++)
+        printf("%f ",a[i]);
+    printf("\n");
 }
